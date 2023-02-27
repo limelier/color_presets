bf486450c27fdbfbeb243754ff4696afc6404dc0
limelier <tudor.iacobescu.1@gmail.com>
2023-02-27 17:54:46 +0200

    Compact agency color buttons to one line

--- ColorPresets/ColorPresets/ColorPresetsMod.cs
+++ ColorPresets/ColorPresets/ColorPresetsMod.cs
@@ -20,50 +20,23 @@ class ColorPickerPatcher
 {
     public static void Postfix(KSP.OAB.ObjectAssemblyColorPicker __instance)
     {
-        // resize the window; TODO: make scrolling instead
-        var rootTransform = (RectTransform) __instance.transform;
-        rootTransform.sizeDelta += new Vector2(0, 150);
+        var agencyColorContainer = __instance.transform
+            .FindChildEx("UIPanel")
+            .FindChildEx("GRP-Body")
+            .FindChildEx("Agency Colors Buttons");
         
-        // create a copy of the agency colors buttons to use for the preset
-        var bodyTransform = __instance.transform.FindChildEx("UIPanel").FindChildEx("GRP-Body");
-        var agencyColorsButtons = bodyTransform.FindChildEx("Agency Colors Buttons");
-        var preset = UnityEngine.Object.Instantiate(agencyColorsButtons.gameObject, bodyTransform);
-        preset.name = "Color presets";
-        var presetTransform = (RectTransform) preset.transform;
-
-        // move the preset
-        presetTransform.SetSiblingIndex(agencyColorsButtons.GetSiblingIndex() + 1);
-        presetTransform.anchoredPosition += new Vector2(0, -150);
+        var horizontalContainer = agencyColorContainer.FindChildEx("GRP-Agency Colors");
         
-        // change the label
-        var label = presetTransform.FindChildEx("PartsManager-ELE-Property Name");
-        label.name = "Label";
-        label.GetComponent<TextMeshProUGUI>()
-            .SetText("Extra color presets");
-        label.SetSiblingIndex(0);
+        // move and rename restore button (use)
+        var useButton = agencyColorContainer.FindChildEx("BTN-RestoreAgencyColors");
+        useButton.SetParent(horizontalContainer);
+        useButton.GetComponentInChildren<TextMeshProUGUI>().SetText("USE");
+        useButton.name = "BTN-UseAgencyColors";
         
-        // change the "set" button to "save new preset"
-        var saveButton = presetTransform.FindChildEx("BTN-SetAgencyColors");
-        saveButton.SetSiblingIndex(1);
-        saveButton.GetComponentInChildren<TextMeshProUGUI>()
-            .SetText("Save new preset");
-        saveButton.name = "BTN-SaveNewPreset";
-        
-        // move the colors
-        var presetContainer = presetTransform.FindChildEx("GRP-Agency Colors");
-        presetContainer.SetSiblingIndex(2);
-        presetContainer.name = "GRP-Preset";
-
-        // move the "restore" button to the colors, change it to "use"
-        var useButton = presetTransform.FindChildEx("BTN-RestoreAgencyColors");
-        useButton.SetParent(presetContainer);
-        useButton.SetSiblingIndex(3);
-        useButton.GetComponentInChildren<TextMeshProUGUI>().SetText("Use");
-        useButton.name = "BTN-Use";
-
-        var removeButton = UnityEngine.Object.Instantiate(useButton.gameObject, presetContainer);
-        removeButton.GetComponentInChildren<TextMeshProUGUI>().SetText("Del");
-        removeButton.name = "BTN-Delete";
+        // move and rename set button
+        var setButton = agencyColorContainer.FindChildEx("BTN-SetAgencyColors");
+        setButton.SetParent(horizontalContainer);
+        setButton.GetComponentInChildren<TextMeshProUGUI>().SetText("SET");
     }
 }
 
